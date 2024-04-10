0960f581b18ed8718120dfb97a6fb1bd74fd98a3
Abraham Navarro <abrahancho6@gmail.com>
2024-04-10 08:17:11 -0400

    feat: versioning test environment variables

--- playwright.config.ts
+++ playwright.config.ts
@@ -75,7 +75,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "npm run seed && npm run dev",
+    command: "npm run seed:e2e && npm run dev",
     env: {
       NODE_ENV: "test"
     },
