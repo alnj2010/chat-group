0495e840e173ac3aaddc6d6ba82ada624bd338c3
Abraham Navarro <abrahancho6@gmail.com>
2024-04-19 15:37:53 -0400

    feat: adding register page fixture for e2e tests

---
+++ tests/e2e/fixtures/register-page.ts
@@ -0,0 +1,30 @@
+import { LOGIN_PAGE_ROUTE } from "@/constanst";
+import { Credentials } from "@/types";
+import { Locator, Page } from "@playwright/test";
+
+export class RegisterPage {
+  private readonly userEmailTextField: Locator;
+  private readonly userPasswordTextField: Locator;
+  private readonly registerButton: Locator;
+
+  constructor(public readonly page: Page) {
+    this.userEmailTextField = this.page.getByTestId("textfield-user-email");
+    this.userPasswordTextField = this.page.getByTestId(
+      "textfield-user-password"
+    );
+    this.registerButton = this.page.getByTestId("register-button");
+  }
+
+  async goto() {
+    await this.page.goto(LOGIN_PAGE_ROUTE);
+    const registerLink = this.page.getByTestId("register-link");
+    await registerLink.click();
+  }
+
+  async submitCredentials(credentials: Credentials) {
+    await this.userEmailTextField.fill(credentials.email);
+    await this.userPasswordTextField.fill(credentials.password);
+    await this.registerButton.click();
+  }
+
+}
--- tests/e2e/register.spec.ts
+++ tests/e2e/register.spec.ts
@@ -1,11 +1,11 @@
 import {
   EDIT_PROFILE_PAGE_ROUTE,
   EMAIL_ALREADY_EXIST,
-  LOGIN_PAGE_ROUTE,
   REGISTER_API_ROUTE,
 } from "@/constanst";
 import { Credentials } from "@/types";
-import { test, expect, Page } from "@playwright/test";
+import { test as base, expect } from "@playwright/test";
+import { RegisterPage } from "./fixtures/register-page";
 
 function getRandomCredentialsByBrowserName(browserName: string): Credentials {
   const id = Math.random().toString().substring(2, 5);
@@ -16,58 +16,51 @@ function getRandomCredentialsByBrowserName(browserName: string): Credentials {
   return credentials;
 }
 
-async function submitCredentials(page: Page, credentials: Credentials) {
-  const userEmailTextField = page.getByTestId("textfield-user-email");
-  const userPasswordTextField = page.getByTestId("textfield-user-password");
+const test = base.extend<{ registerPage: RegisterPage }>({
+  registerPage: async ({ page }, use) => {
+    const registerPage = new RegisterPage(page);
 
-  await userEmailTextField.fill(credentials.email);
-  await userPasswordTextField.fill(credentials.password);
+    registerPage.goto();
 
-  const registerButton = page.getByTestId("register-button");
-  await registerButton.click();
-}
+    use(registerPage);
+  },
+});
 
-test.describe("Register Page", () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto(LOGIN_PAGE_ROUTE);
-    const registerLink = page.getByTestId("register-link");
-    await registerLink.click();
+test("When I correctly fill the register form and press submit button then I will be redirected to edit profile page", async ({
+  registerPage,
+  browserName,
+  page,
+}) => {
+  const credentials = getRandomCredentialsByBrowserName(browserName);
+  await registerPage.submitCredentials(credentials);
+  await page.waitForURL(`**${EDIT_PROFILE_PAGE_ROUTE}`);
+
+  const changeInfoSection = await page.getByTestId("change-info-section");
+
+  const cookies = await page.context().cookies();
+
+  await expect(
+    cookies.find((c) => c.name === "next-auth.session-token")
+  ).toBeDefined();
+  await expect(changeInfoSection).toBeVisible();
+});
+
+test("When I fill the register form and press submit button but the email is duplicated in the system then it will be shown an error message", async ({
+  page,
+  request,
+  browserName,
+  registerPage,
+}) => {
+  const credentials = getRandomCredentialsByBrowserName(browserName);
+
+  await request.post(REGISTER_API_ROUTE, {
+    data: credentials,
   });
 
-  test("When I correctly fill the register form and press submit button then I will be redirected to edit profile page", async ({
-    page,
-    browserName,
-  }) => {
-    const credentials = getRandomCredentialsByBrowserName(browserName);
-    await submitCredentials(page, credentials);
-    await page.waitForURL(`**${EDIT_PROFILE_PAGE_ROUTE}`);
+  await registerPage.submitCredentials(credentials);
+  await page.waitForSelector('[data-testid="error-messages"]');
 
-    const changeInfoSection = await page.getByTestId("change-info-section");
+  const errorMessages = await page.getByTestId("error-messages");
 
-    const cookies = await page.context().cookies();
-
-    await expect(
-      cookies.find((c) => c.name === "next-auth.session-token")
-    ).toBeDefined();
-    await expect(changeInfoSection).toBeVisible();
-  });
-
-  test("When I fill the register form and press submit button but the email is duplicated in the system then it will be shown an error message", async ({
-    page,
-    request,
-    browserName,
-  }) => {
-    const credentials = getRandomCredentialsByBrowserName(browserName);
-
-    await request.post(REGISTER_API_ROUTE, {
-      data: credentials,
-    });
-
-    await submitCredentials(page, credentials);
-    await page.waitForSelector('[data-testid="error-messages"]');
-
-    const errorMessages = await page.getByTestId("error-messages");
-
-    await expect(errorMessages).toContainText(EMAIL_ALREADY_EXIST);
-  });
+  await expect(errorMessages).toContainText(EMAIL_ALREADY_EXIST);
 });
